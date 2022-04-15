f1d1702f57d335ee2f166c78fe3fe72089bffe42
Antonio <antonio.ratsimbazafy@yahoo.com>
2022-04-15 19:30:45 +0300

    Incorporate entity serialization

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ClassSerializerInterceptor, Injectable, SerializeOptions, UseInterceptors } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
-import { response } from 'express';
+import { plainToClass, plainToInstance } from 'class-transformer';
+import {GROUP_ALL_USERS, Users} from './entity/user.entity';
 interface HttpConfig{
   host: string;
   port: number;
@@ -9,14 +10,9 @@ interface HttpConfig{
 }
 
 
-export interface Users{
-  name: Object;
-  email: String;
-  login: Object;
-  registered: Object;
-  picture: Object;
-}
+
 @Injectable()
+@UseInterceptors(ClassSerializerInterceptor)
 export class AppService {
   private iHttp: HttpConfig;
   private iApi: String;
@@ -30,16 +26,32 @@ export class AppService {
     return 'Hello World!';
   }
 
+  @SerializeOptions({
+    groups: [GROUP_ALL_USERS],
+  })
   getUsers(seed: String,nat: String, count: Number): Promise<Users[]>{
-    
    const promise = axios.get(this.iApi+'?seed='+this.iSeed+'&nat='+nat+'&results='+count);
-   const dataPromise: Promise<Users[]> = promise.then((response)=> response.data.results);
+   const dataPromise: Promise<Users[]> = promise.then((response)=> {
+     let data = response.data.results
+    //  data = plainToInstance(Users,data);
+    data =  data.map((item: Users)=>{
+      return {name:item.name,email:item.email,login:item.login,registered:item.registered,picture:item.picture} 
+    })
+    return data;
+    });
    return dataPromise;
   }
 
   getAllUsers(): Promise<Users[]>{
     const promise = axios.get(this.iApi+'?seed='+this.iSeed);
-    const dataPromise = promise.then((response)=> response.data.results);
+    const dataPromise = promise.then((response)=>{
+      let data = response.data.results
+      //  data = plainToInstance(Users,data);
+      data =  data.map((item: Users)=>{
+        return {name:item.name,email:item.email,login:item.login,registered:item.registered,picture:item.picture} 
+      })
+      return data;
+    });
     return dataPromise;
   }
 
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,7 +1,16 @@
+import { Expose } from "class-transformer";
+
+//Class Users as entity
+export const GROUP_ALL_USERS = 'group_all_users';
 export class Users{
+    @Expose({ groups: [GROUP_ALL_USERS] })
     name: Object;
+    @Expose({ groups: [GROUP_ALL_USERS] })
     email: String;
+    @Expose({ groups: [GROUP_ALL_USERS] })
     login: Object;
+    @Expose({ groups: [GROUP_ALL_USERS] })
     registered: Object;
+    @Expose({ groups: [GROUP_ALL_USERS] })
     picture: Object;
 }
